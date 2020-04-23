b01f6d0738d4934973b8727b6223fc38d61d775f
LIMJONGSOO <limjs4535@gmail.com>
2020-04-23 14:08:10 +0900

    mongodb connect and crud router

--- src/components/mongodb-connect-test/service.tsx
+++
@@ -1,6 +0,0 @@
-const { Schema } = mongoose;
-const userSchema = new Schema({  
-    name: {type: String,required: true,unique: true},  
-    age: {    type: Number,    required: true,  },
-});
-module.exports = mongoose.model('User', userSchema);
